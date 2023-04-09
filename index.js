f52c1a4a8758a69fc8c1c20d1b97f32ed13bdafb
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-09 12:45:01 -0400

    Tuples

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var numbers = [1, 2, 3, 3];
-numbers.push(3);
-console.log(numbers);
-numbers.push('c'); //Argument of type 'string' is not assignable to parameter of type 'number'.ts(2345)
+// 1, 'Sandy'
+var users = [1, 'Sandy'];
--- index.ts
+++ index.ts
@@ -1,12 +1,2 @@
-let numbers = [1,2,3,3];
-numbers.push(3);
-console.log(numbers);
-//numbers.push('c'); Argument of type 'string' is not assignable to parameter of type 'number'.ts(2345)
-
-
-    
-
-
-
-
-
+// 1, 'Sandy'
+let users: [number, string] = [1, 'Sandy'];
