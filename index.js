3e79b751929ab0d4267f14a3de52c35abe9c2d14
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-09 01:22:04 -0400

    Type 'string' is not assignable to type 'number'

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello Governor');
+var firstName = "Sandy";
+var age = 34;
+var isADuck = "false";
+var favoriteColor = "blue";
+console.log(age);
+age = "Josh";
+console.log(favoriteColor);
+console.log(firstName);
+console.log(age);
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello Governor')
+let firstName = "Sandy";
+let age = 34;
+let isADuck = "false"
+let favoriteColor = "blue"
+console.log(age);
+age = "Josh";
+console.log(favoriteColor);
+console.log(firstName);
+console.log(age);
