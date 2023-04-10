ff40cd6ea29943d006f4fcd40218036ba63a15a4
Sandy Woods <sandywoods@Sandys-MacBook-Air-2.local>
2023-04-10 01:33:58 -0400

    Intersection Types

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 "use strict";
-function kgToLbs(weight) {
-    //Narrowing Union Type to a specific one
-    if (typeof weight === 'number')
-        return weight * 2.2;
-    else
-        return parseInt(weight) * 2.2;
+{
+    let textBox = {
+        drag: () => { },
+        resize: () => { }
+    };
 }
-kgToLbs(10);
-kgToLbs('10kg');
--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
-function kgToLbs(weight: number | string): number{
-        //Narrowing Union Type to a specific one
-        if(typeof weight === 'number')
-            return weight * 2.2;
-        else 
-            return parseInt(weight) * 2.2;
-        
+type Draggable = {
+    drag: () => void
+};
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable {
+    let textBox: UIWidget = {
+        drag: () => {},
+        resize: () => {}
+    }
 }
-kgToLbs(10);
-kgToLbs('10kg')
